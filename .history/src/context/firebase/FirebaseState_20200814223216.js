7c128abe6f5eeeaff1770abebfb169295994b2ad
TauVip <Abylai1393@gmail.com>
2020-08-14 22:36:18 +0600

    commit 2

---
+++ .history/src/context/firebase/FirebaseState_20200814223216.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FirebaseContext } from './firebaseContext'
+
+export const FirebaseState = ({ children }) => {
+
+  return (
+    <FirebaseContext.Provider>
+      {children}
+    </FirebaseContext.Provider>
+  )
+}
