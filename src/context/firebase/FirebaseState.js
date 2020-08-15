07888cc7f88c187f9e3a6a550ad95376c45b9449
TauVip <Abylai1393@gmail.com>
2020-08-15 10:02:48 +0600

    commit 3

--- src/context/firebase/FirebaseState.js
+++ src/context/firebase/FirebaseState.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import { FirebaseContext } from './firebaseContext'
+import { firebaseReducer } from './firebaseReducer'
 
 export const FirebaseState = ({ children }) => {
+  const initialState = {
+    notes: [],
+    loading: false
+  }
+  const [state, dispatch] = useReducer(firebaseReducer, initialState)
 
   return (
-    <FirebaseContext.Provider>
+    <FirebaseContext.Provider value={{
+      
+    }}>
       {children}
     </FirebaseContext.Provider>
   )
